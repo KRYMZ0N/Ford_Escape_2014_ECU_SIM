76b0c1f6564461a4d8bdc5d13dbab229b28489c1
KRYMZ0N <jackgrimes05@yahoo.com>
2026-07-28 18:07:08 -0400

    Implement HardwareIO class with constructor for LED and buzzer pins

---
+++ src/HardwareIO.cpp
@@ -0,0 +1,4 @@
+#include "HardwareIO.h"
+
+HardwareIO::HardwareIO(uint led, uint buzzer) : ledPin(led), buzzerPin(buzzer) {}
+
--- src/HardwareIO.h
+++ src/HardwareIO.h
@@ -1,2 +1,15 @@
 #pragma once
 #include "pico/stdlib.h"
+
+class HardwareIO {
+private:
+    uint ledPin;
+    uint buzzerPin;
+
+public:
+    HardwareIO(uint led, uint buzzer);
+
+    void init();
+    void triggerDTC();
+    void normalOperation();
+};
